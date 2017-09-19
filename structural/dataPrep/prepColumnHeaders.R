7e0e9e5c97cf20f1675ce8e84229bd6579f33b8a
Adon Rosen <arosen@chead.uphs.upenn.edu>
2017-09-19 12:54:48 -0400

    Added files to prepare struc output

---
+++ structural/dataPrep/prepColumnHeaders.R
@@ -0,0 +1,42 @@
+# Prep the GMD column names
+source('/home/arosen/adroseHelperScripts/R/afgrHelpFunc.R')
+install_load('tools')
+columnValues <- read.csv("/data/joy/BBL/projects/pncReproc2015/pncReproc2015Scripts/jlf/labelList/inclusionCheck.csv")
+gmdValues <- read.table("/data/joy/BBL/projects/mgiReproc2017/structural/mgiGMDVals.1D", header=T)
+nzCols <- grep('NZMean', names(gmdValues))
+nzCols <- append(c(1, 2), nzCols)
+gmdValues <- gmdValues[,nzCols]
+colsOfInterest <- columnValues$Label.Number[which(columnValues$GMD==0)] + 2
+colsOfInterest <- append(c(1,2), colsOfInterest)
+gmdValues <- gmdValues[,colsOfInterest]
+columnNames <- gsub(x=gsub(x=columnValues$JLF.Column.Names, pattern='%MODALITY%', replacement='mprage'), pattern='%MEASURE%', replacement='gmd')[which(columnValues$GMD==0)]
+colnames(gmdValues)[3:length(gmdValues)] <- as.character(columnNames)
+gmdValues[,2] <- strSplitMatrixReturn(gmdValues$subject.1., 'x')[,2]
+colnames(gmdValues)[1:2] <- c('bblid', 'scanid')
+
+# Now do CT
+ctValues <- read.table("/data/joy/BBL/projects/mgiReproc2017/structural/mgiCTVals.1D", header=T)
+nzCols <- grep('NZMean', names(ctValues))
+nzCols <- append(c(1, 2), nzCols)
+ctValues <- ctValues[,nzCols]
+colsOfInterest <- columnValues$Label.Number[which(columnValues$CT==0)] + 2
+colsOfInterest <- append(c(1,2), colsOfInterest)
+ctValues <- ctValues[,colsOfInterest]
+columnNames <- gsub(x=gsub(x=columnValues$JLF.Column.Names, pattern='%MODALITY%', replacement='mprage'), pattern='%MEASURE%', replacement='ct')[which(columnValues$CT==0)]
+colnames(ctValues)[3:length(ctValues)] <- as.character(columnNames)
+ctValues[,2] <- strSplitMatrixReturn(ctValues$subject.1., 'x')[,2]
+colnames(ctValues)[1:2] <- c('bblid', 'scanid')
+
+# And now volume
+volumeValues <- read.table("/data/joy/BBL/projects/mgiReproc2017/structural/mgiVOLVals.1D", header=T)
+colsOfInterest <- columnValues$Label.Number[which(columnValues$Volume==0)] + 2
+colsOfInterest <- append(c(1,2), colsOfInterest)
+volumeValues <- cbind(ctValues$bblid, ctValues$scanid, volumeValues)
+volumeValues <- volumeValues[,colsOfInterest]
+columnNames <- gsub(x=gsub(x=columnValues$JLF.Column.Names, pattern='%MODALITY%', replacement='mprage'), pattern='%MEASURE%', replacement='vol')[which(columnValues$Volume==0)]
+colnames(volumeValues)[3:length(volumeValues)] <- as.character(columnNames)
+colnames(volumeValues)[1:2] <- c('bblid', 'scanid')
+# Now write the output csv's
+write.csv(gmdValues, '/data/joy/BBL/projects/mgiReproc2017/structural/gmdValuesPropHead.csv', quote=F, row.names=F)
+write.csv(ctValues, '/data/joy/BBL/projects/mgiReproc2017/structural/ctValuesPropHead.csv', quote=F, row.names=F)
+write.csv(volumeValues, '/data/joy/BBL/projects/mgiReproc2017/structural/volValuesPropHead.csv', quote=F, row.names=F)
---
+++ structural/dataPrep/prepareMGIFiles.sh
@@ -0,0 +1,8 @@
+/data/joy/BBL/applications/xcpEngine/utils/combineOutput -p /data/joy/BBL/studies/MGI2/processedData/structural/nkiTemp/mgiNKITempTest_201708111144/ -f "JLFIntersect_val_gmSegIntersect.1D" -o "mgiGMDVals.1D"
+/data/joy/BBL/applications/xcpEngine/utils/combineOutput -p /data/joy/BBL/studies/MGI2/processedData/structural/nkiTemp/mgiNKITempTest_201708111144/ -f "JLFIntersect_val_vol_volume.1D" -o "mgiVOLVals.1D"
+/data/joy/BBL/applications/xcpEngine/utils/combineOutput -p /data/joy/BBL/studies/MGI2/processedData/structural/nkiTemp/mgiNKITempTest_201708111144/ -f "JLFIntersect_val_corticalThickness.1D" -o "mgiCTVals.1D"
+
+mv /data/joy/BBL/studies/MGI2/processedData/structural/nkiTemp/mgiNKITempTest_201708111144/*1D /data/joy/BBL/projects/mgiReproc2017/structural
+
+# Now write the output CSV's
+Rscript /data/joy/BBL/projects/mgiReproc2017/mgiReproc2017Scripts/structural/dataPrep/prepColumnHeaders.R
