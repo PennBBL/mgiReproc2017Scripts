ea346a7fde3d52b571bbc0b208583325834ca742
Adon Rosen <arosen@chead.uphs.upenn.edu>
2017-10-05 09:21:07 -0400

    Added cortcon exports

--- structural/dataPrep/prepColumnHeaders.R
+++ structural/dataPrep/prepColumnHeaders.R
@@ -27,6 +27,19 @@ colnames(ctValues)[3:length(ctValues)] <- as.character(columnNames)
 ctValues[,2] <- strSplitMatrixReturn(ctValues$subject.1., 'x')[,2]
 colnames(ctValues)[1:2] <- c('bblid', 'scanid')
 
+# Now do CC
+ccValues <- read.table("/data/joy/BBL/projects/mgiReproc2017/structural/mgiCCVals.1D", header=T)
+nzCols <- grep('NZMean', names(ccValues))
+nzCols <- append(c(1, 2), nzCols)
+ccValues <- ccValues[,nzCols]
+colsOfInterest <- columnValues$Label.Number[which(columnValues$CT==0)] + 2
+colsOfInterest <- append(c(1,2), colsOfInterest)
+ccValues <- ccValues[,colsOfInterest]
+columnNames <- gsub(x=gsub(x=columnValues$JLF.Column.Names, pattern='%MODALITY%', replacement='mprage'), pattern='%MEASURE%', replacement='cortcon')[which(columnValues$CT==0)]
+colnames(ccValues)[3:length(ccValues)] <- as.character(columnNames)
+ccValues[,2] <- strSplitMatrixReturn(ccValues$subject.1., 'x')[,2]
+colnames(ccValues)[1:2] <- c('bblid', 'scanid')
+
 # And now volume
 volumeValues <- read.table("/data/joy/BBL/projects/mgiReproc2017/structural/mgiVOLVals.1D", header=T)
 colsOfInterest <- columnValues$Label.Number[which(columnValues$Volume==0)] + 2
@@ -40,3 +53,4 @@ colnames(volumeValues)[1:2] <- c('bblid', 'scanid')
 write.csv(gmdValues, '/data/joy/BBL/projects/mgiReproc2017/structural/gmdValuesPropHead.csv', quote=F, row.names=F)
 write.csv(ctValues, '/data/joy/BBL/projects/mgiReproc2017/structural/ctValuesPropHead.csv', quote=F, row.names=F)
 write.csv(volumeValues, '/data/joy/BBL/projects/mgiReproc2017/structural/volValuesPropHead.csv', quote=F, row.names=F)
+write.csv(ccValues, '/data/joy/BBL/projects/mgiReproc2017/structural/ccValuesPropHead.csv', quote=F, row.names=F)
--- structural/dataPrep/prepareMGIFiles.sh
+++ structural/dataPrep/prepareMGIFiles.sh
@@ -1,6 +1,7 @@
 /data/joy/BBL/applications/xcpEngine/utils/combineOutput -p /data/joy/BBL/studies/MGI2/processedData/structural/nkiTemp/mgiNKITempTest_201708111144/ -f "JLFIntersect_val_gmSegIntersect.1D" -o "mgiGMDVals.1D"
 /data/joy/BBL/applications/xcpEngine/utils/combineOutput -p /data/joy/BBL/studies/MGI2/processedData/structural/nkiTemp/mgiNKITempTest_201708111144/ -f "JLFIntersect_val_vol_volume.1D" -o "mgiVOLVals.1D"
 /data/joy/BBL/applications/xcpEngine/utils/combineOutput -p /data/joy/BBL/studies/MGI2/processedData/structural/nkiTemp/mgiNKITempTest_201708111144/ -f "JLFIntersect_val_corticalThickness.1D" -o "mgiCTVals.1D"
+/data/joy/BBL/applications/xcpEngine/utils/combineOutput -p /data/joy/BBL/studies/MGI2/processedData/structural/nkiTemp/mgiNKITempTest_201708111144/ -f "JLFIntersect_val_cortConVals.1D" -o "mgiCCVals.1D"
 
 mv /data/joy/BBL/studies/MGI2/processedData/structural/nkiTemp/mgiNKITempTest_201708111144/*1D /data/joy/BBL/projects/mgiReproc2017/structural
 
